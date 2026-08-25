798d54324c712ed5ac9e9892a16646a918736976
Hem Ranjan <hemranjan49@gmail.com>
2026-08-26 00:37:41 +0530

    updated today

--- JS/JS_MiniProject/app.js
+++ JS/JS_MiniProject/app.js
@@ -15,12 +15,22 @@ btn.addEventListener("click", function() {
     inp.value = "";
 });
 
-let delBtns = document.querySelectorAll(".delete");
+//Event bubbling:-
 
-for (delBtn of delBtns) {
-    delBtn.addEventListener("click", function() {
-        let par = this.parentElement;
-        console.log(par);
-        par.remove();
-    });
-}
+ul.addEventListener("click", function(event) {
+    if (event.target.nodeName == "BUTTON") {
+        let listItem = event.target.parentElement;
+        listItem.remove();
+    }
+});
+
+
+// let delBtns = document.querySelectorAll(".delete");
+
+// for (delBtn of delBtns) {
+//     delBtn.addEventListener("click", function() {
+//         let par = this.parentElement;
+//         console.log(par);
+//         par.remove();
+//     });
+// }
