92fd6f2147e3d23d3ff7b6e716b31534ec69632c
Hem Ranjan <hemranjan49@gmail.com>
2026-08-26 00:47:03 +0530

    updated today

--- JS/JS_MiniProject/app.js
+++ JS/JS_MiniProject/app.js
@@ -2,6 +2,7 @@ let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
 let inp = document.querySelector("input");
 
+//To add items after typing in input bar:-
 btn.addEventListener("click", function() {
     let item = document.createElement("li");
     item.innerText = inp.value;
