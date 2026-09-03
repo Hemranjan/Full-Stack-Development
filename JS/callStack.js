7894e07d09570839a95ddfeca7bd259ad15e2bec
Hem Ranjan <hemranjan49@gmail.com>
2026-09-03 23:09:04 +0530

    updated today

---
+++ JS/callStack.js
@@ -0,0 +1,17 @@
+// function hello () {
+//     console.log("hello");
+// }
+
+// hello();
+
+function first() {
+  console.log("First");
+  second();
+  console.log("First ends");
+}
+
+function second() {
+  console.log("Second");
+}
+
+first();
