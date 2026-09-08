713c069ef5a0af2b5eac6a4e63a6909f8d1e4c04
Hem Ranjan <hemranjan49@gmail.com>
2026-09-08 23:39:59 +0530

    updated

--- Promises/then_catch.js
+++ Promises/then_catch.js
@@ -1,4 +1,17 @@
-let request = saveToDBPromise("apnacollege");
+function saveToDb(data) {
+    return new Promise((resolve, reject) => {
+        let internetSpeed = Math.floor(Math.random() * 10) + 1;
+
+        if (internetSpeed > 4) {
+            resolve("success : data was saved");
+        } else {
+            reject("failure : weak connection");
+        }
+    });
+}
+
+
+let request = saveToDb("apnacollege");
 
 request
     .then(() => {
@@ -6,4 +19,4 @@ request
     })
     .catch(() => {
         console.log("promise rejected");
-    });
+});
